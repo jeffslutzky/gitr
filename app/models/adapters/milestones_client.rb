a19f244f28928b6470de315f6bfb01d5338e409d
reubenson <reubenson@gmail.com>
2015-12-09 23:25:10 -0500

    added commit model and method to grab commits for repos using github api ..

---
+++ app/models/adapters/milestones_client.rb
@@ -0,0 +1,24 @@
+module Adapters
+  class MilestonesClient
+
+    def connection
+      @connection = Adapters::GithubConnection.new
+    end
+
+    def get_milestones_for_project(username, project)
+      milestones = connection.query(username, project.name,'milestones')
+      milestones.each do |milestone|
+        milestone_creator_uid = milestone["creator"]["id"]
+        milestone_creator = User.find_by(uid: milestone_creator_uid)
+        # the following does not necessarily account for a milestone that gets modified
+        milestone = Milestone.find_or_create_by(
+          title: milestone["title"],
+          description: milestone["description"],
+          state: milestone["state"],
+          project_id: project.id,
+          collaborator_id: milestone_creator.collaborator.id
+        )
+      end
+    end
+  end
+end
