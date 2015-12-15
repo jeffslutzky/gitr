bbca7093d1dcb4c97f47fd1bfea105e1403f2437
reubenson <reubenson@gmail.com>
2015-12-15 10:27:17 -0500

    binding back

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
   end
 
   def event_handler
-    # binding.pry
+    binding.pry
     ref = params["ref"]
     ref_type = params["ref_type"]
     repo_id = params["repository"]["id"]
